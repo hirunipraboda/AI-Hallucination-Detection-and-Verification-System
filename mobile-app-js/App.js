e7d76cb5425bc5b73031ac84df538c6193e9471b
Timali Jayasinghe <timalinayodara@gmail.com>
2026-02-26 03:49:18 +0530

    Initila setup: added navigation and folder structure

---
+++ mobile-app-js/App.js
@@ -0,0 +1,4 @@
+import { NavigationContainer} from '@react-navigation/native';
+import { createBottonTabNavigator } from '@react-navigation/bottom-tabs';
+import{Text} from 'react-native';
+import SourcesListScreen from './screens/SourcesListScreen'
